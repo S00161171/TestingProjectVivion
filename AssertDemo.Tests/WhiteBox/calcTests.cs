fc90f8997f56e9b3fc4adaa334fa28fee81ec89a
Josh Butler <31965183+S00161171@users.noreply.github.com>
2018-02-27 15:33:23 +0000

    push

---
+++ AssertDemo.Tests/WhiteBox/calcTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PRG.Tests.Equality
+{
+    [TestFixture]
+    public class calcTests
+    {
+        [Test]
+        public void TwentyYearOldMale()
+        {
+            var sut = new CalcPremium();
+
+            var result = sut.calc(20, "male");
+
+            Assert.That(result, Is.EqualTo(6.0));
+        }
+        [Test]
+        public void SixtyYearOldMale()
+        {
+            var sut = new CalcPremium();
+
+            var result = sut.calc(60, "male");
+
+            Assert.That(result, Is.EqualTo(0.75));
+        }
+        [Test]
+        public void FifteenYearOldMale()
+        {
+            var sut = new CalcPremium();
+
+            var result = sut.calc(15, "male");
+
+            Assert.That(result, Is.EqualTo(0.0));
+        }
+
+
+        [Test]
+        public void TwentyYearOldFemale()
+        {
+            var sut = new CalcPremium();
+
+            var result = sut.calc(20, "female");
+
+            Assert.That(result, Is.EqualTo(5.0));
+        }
+
+        [Test]
+        public void SixtyYearOldFemale()
+        {
+            var sut = new CalcPremium();
+
+            var result = sut.calc(60, "female");
+
+            Assert.That(result, Is.EqualTo(0.375));
+        }
+        [Test]
+        public void FifteenYearOldFemale()
+        {
+            var sut = new CalcPremium();
+
+            var result = sut.calc(15, "female");
+
+            Assert.That(result, Is.EqualTo(0.0));
+        }
+
+
+
+        [Test]
+        public void TwentyYearOldNonGender()
+        {
+            var sut = new CalcPremium();
+
+            var result = sut.calc(20, "na");
+
+            Assert.That(result, Is.EqualTo(0.0));
+        }
+
+    }
+}
---
+++ AssertDemo/CalcPremium.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using fit;
+using fit.Fixtures;
+
+namespace PRG
+{
+    public class CalcPremium
+    {
+
+        public float calc(int age, string gender)
+        {
+            float premium;
+
+            if (gender == "female")
+                if ((age >= 18) && (age <= 30))
+                    premium = (float)5.0;
+                else
+                    if (age >= 31)
+                        premium = (float)2.50;
+                    else
+                        premium = (float)0.0;
+            else if (gender == "male")
+                    if ((age >= 18) && (age <= 35))
+                        premium = (float)6.0;
+                    else
+                       if (age >= 36)
+                            premium = (float)5.0;
+                       else
+                            premium = (float)0.0;
+            else
+                premium = (float)0.0;
+
+            if (age >= 50)
+                premium = premium * (float)0.15;
+            return premium;
+        }
+
+    }
+    public class PRG : ColumnFixture
+    {
+        public float premium;
+        public string gender;
+        public int age;
+
+        public CalcPremium calculator = new CalcPremium();
+
+        public float BlackBoxTesting()
+        {
+            return calculator.calc(this.age, this.gender);
+        }
+
+    }
+
+}
